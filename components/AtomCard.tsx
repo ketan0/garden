b293f988949a40d443cdfe160ea18645e3a87a14
Ketan Agrawal <agrawalk@stanford.edu>
2021-01-02 04:39:40 -0500

    display atoms as "card"

--- components/AtomCard.tsx
+++ components/AtomCard.tsx
@@ -1,24 +1,21 @@
 import Link from 'next/Link'
+import { AtomCardInfo } from '../types';
 
-export interface AtomInfo {
-  id: string
-  title: string
-  contents: string
-}
-
-const AtomCard = ({ id, title, contents }: AtomInfo) => {
+const AtomCard = ({ id, title, contents, isActive }: AtomCardInfo) => {
   const linkString = `/atoms/${id}`;
   return (
-    <Link href={linkString}>
-      <a style={{ textDecoration: 'none' }}>
-        <div className="card">
-          <div className="container">
-            <h4><b>{title}</b></h4>
-            <p>{contents}</p>
+    <div className={`${isActive ? 'border-2 border-yellow-300' : ''} bg-white hover:bg-yellow-100 shadow-md rounded-xl p-4 m-2`} >
+      <Link href={linkString}>
+        <a style={{ color: 'inherit', textDecoration: 'none' }}>
+          <div>
+            <div>
+              <p className="text-lg font-semibold">{title}</p>
+              <p className="font-sans">{contents}</p>
+            </div>
           </div>
-        </div>
-      </a>
-    </Link >
+        </a>
+      </Link >
+    </div >
   )
 }
 
