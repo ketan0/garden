b688d133c06cf140ce7bb4f003ef06149134308e
Ketan Agrawal <agrawalk@stanford.edu>
2021-01-02 04:36:54 -0500

    display atom in "outline" hierarchy w/ parents + children

--- pages/atoms/[id].tsx
+++ pages/atoms/[id].tsx
@@ -1,7 +1,10 @@
+import Nav from '../../components/Nav'
 import { useRouter } from 'next/router'
+import Link from 'next/Link'
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import AtomCard, { AtomInfo } from '../../components/AtomCard'
+import AtomCard from '../../components/AtomCard'
+import { AtomInfo } from '../../types';
 
 const AtomPage = () => {
   const router = useRouter()
@@ -15,8 +18,8 @@ const AtomPage = () => {
         const results = await axios.get('/api/getAtomAndChildren', {
           params: { atomId: id }
         })
-        console.log('results:')
-        console.log(JSON.stringify(results, undefined, 2))
+        // console.log('results:')
+        // console.log(JSON.stringify(results, undefined, 2))
         if (results.status != 200) {
           console.error(results.data.message)
         } else {
@@ -33,11 +36,15 @@ const AtomPage = () => {
     }
   }, [id])
 
-  console.log(`id in page: ${id}`)
+
+  // console.log(`id in page: ${id}`)
   return (
     <div>
-      <h1>Atom:</h1>
-      {atom && <AtomCard id={atom.id} title={atom.title} contents={atom.contents} />}
+      <Nav />
+      {id &&
+        <Link href={`/graph/${id}`}>
+          <a>Graph View</a>
+        </Link>}
       {atomParent &&
         <>
           <h1>Parent:</h1>
@@ -49,8 +56,15 @@ const AtomPage = () => {
           />
         </>
       }
+      {atom &&
+        <div className="ml-10">
+          <h1>Atom:</h1>
+
+          <AtomCard id={atom.id} title={atom.title} contents={atom.contents} isActive />
+        </div>
+      }
       {atomChildren && atomChildren.length > 0 &&
-        <>
+        (<div className="ml-20">
           <h1>Children:</h1>
           {atomChildren.map(({ id, title, contents }) => (
             <AtomCard
@@ -60,8 +74,7 @@ const AtomPage = () => {
               key={id}
             />
           ))}
-        </>
-
+        </div>)
       }
     </div>
   )
